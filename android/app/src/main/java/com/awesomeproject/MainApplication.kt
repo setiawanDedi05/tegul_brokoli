41a7796294627e34ad4c968e78066f11432f34f1
setiawanDedi05 <dssetiawan05@gmail.com>
2025-04-14 07:25:21 +0700

    init renaming application

--- android/app/src/main/java/com/awesomeproject/MainApplication.kt
+++ android/app/src/main/java/com/awesomeproject/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.awesomeproject
+package com.tegulBrokoli
 
 import android.app.Application
 import com.facebook.react.PackageList
--- src/App.tsx
+++ src/App.tsx
@@ -18,10 +18,6 @@ import {
 
 import {
   Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
 type SectionProps = PropsWithChildren<{
@@ -80,29 +76,14 @@ function App(): React.JSX.Element {
       />
       <ScrollView
         style={backgroundStyle}>
-        <View style={{paddingRight: safePadding}}>
-          <Header/>
-        </View>
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
-            paddingHorizontal: safePadding,
-            paddingBottom: safePadding,
+            padding: safePadding
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
+          <Section title="Tegul Brokoli">
+            Hallo Brokoli
           </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
         </View>
       </ScrollView>
     </View>
